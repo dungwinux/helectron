7d49373fa616b2707fd9d4adcb968a349eb1e383
Minh Duc Vo <minhducsun2002@gmail.com>
2018-09-08 21:10:52 +0700

    Add more features
    Memory state added
    Temporary directory added
    CPU endianness added
    Some architectures now have human-readable name
    
    Removed CPU speed for proper implementation

--- asset/index/index.js
+++ asset/index/index.js
@@ -4,11 +4,31 @@ const hostName = os.hostname()
 
 const sysOs = os.type()
 const sysRelease = os.release()
+const sysEndian = (os.endianness() == "BE") ? "big endian" : "little endian"
+const tempDir = os.tmpdir();
 
 const sysCpuModel = os.cpus()[0].model
-const sysCpuSpeed = os.cpus()[0].speed
 const sysCpuCores = os.cpus().length
-const sysCpuArch = process.arch
+const sysCpuArch = convertArch(process.arch)
+const sysMemory = Math.floor(os.totalmem() / 1048576) // Convert to MiB
+
+var sysFreeMem = Math.floor(os.freemem() / 1048576) // Convert to MiB
+setInterval(() => {
+    sysFreeMem = Math.floor(os.freemem() / 1048576);
+}, 400)
+
+function convertArch(arg)
+{
+    if (arg == "x64") return "AMD64/Intel 64";
+    if (arg == "ia32") return "IA-32";
+    if (arg == "x32") return "AMD64/Intel 64 with 32-bit pointers & values"
+    if (arg == "s390") return "IBM System/390";
+    if (arg == "arm") return "ARM version 7"
+    if (arg == "arm64") return "ARM 64-bit";
+    if (arg == "mips") return "MIPS";
+    if (arg == "mipsel") return "MIPS little-endian";
+    return arg;
+}
 
 function PrintNetworkInterface() {
     var netintf, infname, infconf, add
--- asset/index/script.js
+++ asset/index/script.js
@@ -4,6 +4,13 @@ document.getElementById("ostype").innerHTML = sysOs
 document.getElementById("release").innerHTML = sysRelease
 
 document.getElementById("cpuModel").innerHTML = sysCpuModel
-document.getElementById("cpuSpeed").innerHTML = sysCpuSpeed
 document.getElementById("cpuCores").innerHTML = sysCpuCores
 document.getElementById("cpuArch").innerHTML = sysCpuArch
+document.getElementById("cpuEndian").innerHTML = sysEndian
+
+document.getElementById("tempDirectory").innerHTML = tempDir
+document.getElementById("sysMemory").innerHTML = sysMemory
+
+setInterval(() => {
+    document.getElementById("freeMemory").innerHTML = sysFreeMem;
+}, 500);
