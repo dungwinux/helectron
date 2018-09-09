4bde2011ae3b32deab1c604db837be29af244a4d
Dung Winux <ntddebugger@gmail.com>
2018-09-09 16:44:10 +0700

    Fix unsafe comparing

--- asset/index/update.js
+++ asset/index/update.js
@@ -1,11 +1,11 @@
-var sysFreeMem = Math.floor(os.freemem() / 1048576) // Convert to MiB
+var sysFreeMem = Math.floor(os.freemem() / 1048576); // Convert to MiB
 var sysUptime = timestr(os.uptime());
 setInterval(() => {
     sysFreeMem = Math.floor(os.freemem() / 1048576);
-}, 400) // update 
+}, 400); // update
 setInterval(() => {
     sysUptime = timestr(os.uptime());
-}, 500) // update
+}, 500); // update
 
 /**
  * attributed to github.com/NOVAglow
@@ -15,32 +15,48 @@ setInterval(() => {
  @return {string} s converted to long format
  */
 function timestr(s) {
-    w = Math.trunc(s / 60 / 60 / 24 / 7);  // Number of weeks
-    d = Math.trunc(s / 60 / 60 / 24) % 7;  // Number of days
-    h = Math.trunc(s / 60 / 60) % 24;      // Number of hours
-    m = Math.trunc(s / 60) % 60;           // Number of minutes
-    s = s % 60;                            // Number of seconds
+    w = Math.trunc(s / 60 / 60 / 24 / 7);   // Number of weeks
+    d = Math.trunc(s / 60 / 60 / 24) % 7;   // Number of days
+    h = Math.trunc(s / 60 / 60) % 24;       // Number of hours
+    m = Math.trunc(s / 60) % 60;            // Number of minutes
+    s = s % 60;                             // Number of seconds
 
     result = "";
     if (w > 0) {
         result = w + " week" + (w == 1 ? "" : "s");
     }
 
-    result = result + (result != "" ? ", " + d + " day" + (d > 1 ? "s" : "")
-                                    : (d > 0 ? d + " day" + (d > 1 ? "s" : "")
-                                             : ""));
+    result =
+        result +
+        (result !== ""
+            ? ", " + d + " day" + (d > 1 ? "s" : "")
+            : d > 0
+                ? d + " day" + (d > 1 ? "s" : "")
+                : "");
 
-    result = result + (result != "" ? ", " + h + " hour" + (h > 1 ? "s" : "")
-                                    : (h > 0 ? h + " hour" + (h > 1 ? "s" : "")
-                                             : ""));
+    result =
+        result +
+        (result !== ""
+            ? ", " + h + " hour" + (h > 1 ? "s" : "")
+            : h > 0
+                ? h + " hour" + (h > 1 ? "s" : "")
+                : "");
 
-    result = result + (result != "" ? ", " + m + " minute" + (m > 1 ? "s" : "")
-                                    : (m > 0 ? m + " minute" + (m > 1 ? "s" : "")
-                                             : ""));
+    result =
+        result +
+        (result !== ""
+            ? ", " + m + " minute" + (m > 1 ? "s" : "")
+            : m > 0
+                ? m + " minute" + (m > 1 ? "s" : "")
+                : "");
 
-    result = result + (result != "" ? ", " + s + " second" + (s > 1 ? "s" : "")
-                                    : (s > 0 ? s + " second" + (s > 1 ? "s" : "")
-                                             : ""));
+    result =
+        result +
+        (result !== ""
+            ? ", " + s + " second" + (s > 1 ? "s" : "")
+            : s > 0
+                ? s + " second" + (s > 1 ? "s" : "")
+                : "");
 
     return result;
 }
