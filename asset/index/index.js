7b3935d47dd861efa6f2136e354f3c0e0433ce34
Minh Duc Vo <minhducsun2002@gmail.com>
2018-09-09 09:01:26 +0700

    Code clean up
    Group calls into functions
    Removed old UIs
    Moved some functions into separate files

--- asset/index/index.js
+++ asset/index/index.js
@@ -9,16 +9,9 @@ const tempDir = os.tmpdir();
 const sysCpuModel = os.cpus()[0].model
 const sysCpuCores = os.cpus().length
 const sysCpuArch = convertArch(os.arch())
-const sysPlatform = convertPlatform(os.platform());
+const sysPlatform = convertPlatform(os.platform())
 const sysMemory = Math.floor(os.totalmem() / 1048576) // Convert to MiB
-var sysFreeMem = Math.floor(os.freemem() / 1048576) // Convert to MiB
-var sysUptime = timestr(os.uptime());
-setInterval(() => {
-    sysFreeMem = Math.floor(os.freemem() / 1048576);
-}, 400) // update 
-setInterval(() => {
-    sysUptime = timestr(os.uptime());
-}, 400) // update
+
 
 const currentUser = os.userInfo();
 
@@ -33,44 +26,6 @@ function convertArch(arg)
     if (arg == "mips") return "MIPS";
     if (arg == "mipsel") return "MIPS little-endian";
     return arg;
-}
-
-/**
- * attributed to github.com/NOVAglow
- Convert seconds to long format, from weeks down to seconds
-
- @param {integer} s Given number of seconds
- @return {string} s converted to long format
- */
-function timestr(s) {
-    w = Math.trunc(s / 60 / 60 / 24 / 7);  // Number of weeks
-    d = Math.trunc(s / 60 / 60 / 24) % 7;  // Number of days
-    h = Math.trunc(s / 60 / 60) % 24;      // Number of hours
-    m = Math.trunc(s / 60) % 60;           // Number of minutes
-    s = s % 60;                            // Number of seconds
-
-    result = "";
-    if (w > 0) {
-        result = w + " week" + (w == 1 ? "" : "s");
-    }
-
-    result = result + (result != "" ? ", " + d + " day" + (d > 1 ? "s" : "")
-                                    : (d > 0 ? d + " day" + (d > 1 ? "s" : "")
-                                             : ""));
-
-    result = result + (result != "" ? ", " + h + " hour" + (h > 1 ? "s" : "")
-                                    : (h > 0 ? h + " hour" + (h > 1 ? "s" : "")
-                                             : ""));
-
-    result = result + (result != "" ? ", " + m + " minute" + (m > 1 ? "s" : "")
-                                    : (m > 0 ? m + " minute" + (m > 1 ? "s" : "")
-                                             : ""));
-
-    result = result + (result != "" ? ", " + s + " second" + (s > 1 ? "s" : "")
-                                    : (s > 0 ? s + " second" + (s > 1 ? "s" : "")
-                                             : ""));
-
-    return result;
 }
 
 function convertPlatform(arg)
---
+++ asset/index/present.js
@@ -0,0 +1,48 @@
+function setConstants()
+{
+    /*
+        System params
+    */
+    document.getElementById("hostname").innerHTML = hostName
+
+    document.getElementById("ostype").innerHTML = sysPlatform
+    if (sysPlatform.includes("Linux")) document.querySelector("#linux-name").innerHTML = "Kernel"
+
+    document.getElementById("release").innerHTML = sysRelease
+    document.getElementById("cpuModel").innerHTML = sysCpuModel
+    document.getElementById("cpuCores").innerHTML = sysCpuCores
+    document.getElementById("cpuArch").innerHTML = sysCpuArch
+    document.getElementById("cpuEndian").innerHTML = sysEndian
+
+    document.getElementById("tempDirectory").innerHTML = tempDir
+    document.getElementById("sysMemory").innerHTML = sysMemory
+
+    /*
+        User params
+    */
+    document.getElementById("username").innerHTML = currentUser["username"]
+    document.getElementById("homedir").innerHTML = currentUser["homedir"] + '/'
+
+    if (sysPlatform.includes("Windows")) // if OS is Windows, this is irrelevant
+        document.querySelector("#posix-user").style.display = "none"
+    else 
+    {
+        document.getElementById("username").setAttribute("class", "code")
+        document.getElementById("uid").innerHTML = currentUser["uid"]
+        document.getElementById("gid").innerHTML = currentUser["gid"]
+        document.getElementById("login-shell").innerHTML = currentUser["shell"]
+    }
+}
+
+function initializeUpdate()
+{
+    setInterval(() => {
+        document.getElementById("freeMemory").innerHTML = sysFreeMem;
+    }, 500) // update
+    setInterval(() => {
+        document.getElementById("uptime").innerHTML = sysUptime;
+    }, 500) // update    
+}
+
+setConstants();
+initializeUpdate();
